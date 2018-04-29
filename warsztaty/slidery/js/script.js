d33c17bb75a49cdaaf8124b6790f6716f3a1e5f0
Adrian Jobda <adi.jobda@gmail.com>
2018-04-29 16:10:36 +0200

    Testy

--- warsztaty/slidery/js/script.js
+++ warsztaty/slidery/js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+$(function(){
+    function slide(){
+    $('.fa-arrow-right').click(function(){
+        $('#allSliders').animate({right:'100vw'}, 2000, slide());
+    });
+    $('.fa-arrow-left').click(function(){
+        $('#allSliders').animate({left:'100vw'}, 2000, slide());
+    });
+    };
+});
