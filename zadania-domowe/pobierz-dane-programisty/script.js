cf67e6ba18744ca3a662385ce675f9caae1e03bf
Adrian Jobda <adi.jobda@gmail.com>
2018-05-09 19:14:46 +0200

    Wykonanie zadania

---
+++ zadania-domowe/pobierz-dane-programisty/script.js
@@ -0,0 +1,11 @@
+'use strict';
+$(function () {
+    $('#pobierz').on('click', function () {
+        $.getJSON(
+            "https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php",
+            function (data) {
+                $('#dane-programisty').append("Imię i nazwisko: " + data.imie + " " + data.nazwisko + "<br>Zawód: " + data.zawod + "<br> Firma: " + data.firma);
+            }
+        );
+    })
+});
