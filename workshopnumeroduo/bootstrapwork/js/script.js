e74500be1b2eb7ce1a1144933f7edc9aa0facc9f
Adrian Jobda <adi.jobda@gmail.com>
2018-05-13 14:27:27 +0200

    Zatrzymanie akcji przycisku

---
+++ workshopnumeroduo/bootstrapwork/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+$(function(){
+    
+    $('#wyslij').on('click', function(event){
+        event.preventDefault();
+        alert('Dziękujemy za wysłanie formularza!');
+    });
+    
+});
