8cacbf33625061fec895a3cc5cfb4df020fb2bfd
Adrian Jobda <adi.jobda@gmail.com>
2018-05-13 15:05:03 +0200

    Beauty

--- workshopnumeroduo/bootstrapwork/js/script.js
+++ workshopnumeroduo/bootstrapwork/js/script.js
@@ -1,22 +1,18 @@
 'use strict';
-$(function(){
-    
-    $('#wyslij').on('click', function(event){
+$(function () {
+    $('#wyslij').on('click', function (event) {
         event.preventDefault();
         alert('Dziękujemy za wysłanie formularza!');
     });
-    
-    $(window).on('scroll', function(){
-    
-    var wysokoscOkna = window.innerHeight;
-    var scroll = document.documentElement.scrollTop;
-   if (scroll >= wysokoscOkna-50){
-       $('#main-nav').removeClass('navbarunscrolled')
-       $('#main-nav').addClass('navbarscrolled')
-   }
-        else{
-             $('#main-nav').removeClass('navbarscrolled')
-       $('#main-nav').addClass('navbarunscrolled')
+    $(window).on('scroll', function () {
+        var wysokoscOkna = window.innerHeight;
+        var scroll = document.documentElement.scrollTop;
+        if (scroll >= wysokoscOkna - 50) {
+            $('#main-nav').removeClass('navbarunscrolled')
+            $('#main-nav').addClass('navbarscrolled')
+        } else {
+            $('#main-nav').removeClass('navbarscrolled')
+            $('#main-nav').addClass('navbarunscrolled')
         };
     });
 });
