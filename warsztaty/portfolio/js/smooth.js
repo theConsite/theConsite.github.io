e4a67a8ba404f4ac750f0ac71d8d4f1d80992e4a
Adrian Jobda <adi.jobda@gmail.com>
2018-05-19 21:38:47 +0200

    Beauty

--- warsztaty/portfolio/js/smooth.js
+++ warsztaty/portfolio/js/smooth.js
@@ -1,6 +1,6 @@
-$(function(){ 
-$('a[href*="#"]:not([href="#"])').click(function (e) {
-    'use strict';
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function (e) {
+        'use strict';
         $('html, body').animate({
             scrollTop: $(this.hash).offset().top
         }, 500)
