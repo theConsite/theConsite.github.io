6dd9e36578254592d35b10be9b0521c0acbae14f
Adrian Jobda <adi.jobda@gmail.com>
2018-05-21 20:45:35 +0200

    Poczatek struktury

---
+++ workshopnumeroduo/portfoliosass/app/js/smooth.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function (e) {
+        'use strict';
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 500)
+    });
+});
